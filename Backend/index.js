47ff9adfa4fa2bf2330d8f6a8a34b760e11b2eb7
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2024-09-13 12:32:18 +0500

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,25 @@
+// import express from "express" ;
+// const app = express() ;
+
+// app.get('/',(req,res)=>{
+//   res.send("server is ready") ;
+// })
+
+// const port = process.env.PORT|| 3000 ;
+
+// app.listen(port,()=>{
+//   console.log('i am serving backend services');
+  
+// })
+
 import express from "express" ;
 const app = express() ;
-
-app.get('/',(req,res)=>{
-  res.send("server is ready") ;
+app.get('/',(req, res)=>{
+  res.send('server running') ;
 })
 
-const port = process.env.PORT|| 3000 ;
-
+const port = process.env.port || 3000 ;
 app.listen(port,()=>{
-  console.log('i am serving backend services');
+  console.log('learning bacend');
   
 })
